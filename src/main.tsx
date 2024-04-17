42df8de97a7cfe9df8b97cf595ee811cd8598958
lik3as <daviacosta2006@gmail.com>
2024-04-16 22:05:32 -0300

    removed app and using only main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import GlobalStyles from "./utils/styles";
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles/>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
