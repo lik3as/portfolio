00ed7a714be15d0ef2d38f4b3f9654609f6a8202
lik3as <daviacosta2006@gmail.com>
2024-04-16 22:03:57 -0300

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const LINKEDIN_URL = "https://linkedin.com/in/davi-almeida-da-costa-85a12323a";
+export const WHATSAPP_URL = "https://wa.me/+5592992508190/?text=Consegui seu contato pelo portfólio.";
+export const GITHUB_URL = "https://github.com/lik3as";
+
+export const HEADER_HEIGHT = 62;
