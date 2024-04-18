5d64088abbab3f87ec3dbec8e6776ef34e8d613c
lik3as <daviacosta2006@gmail.com>
2024-04-18 19:58:07 -0300

    add new colors for tech stacks

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,14 @@ export const LINKEDIN_URL = "https://linkedin.com/in/davi-almeida-da-costa-85a12
 export const WHATSAPP_URL = "https://wa.me/+5592992508190/?text=Consegui seu contato pelo portfólio.";
 export const GITHUB_URL = "https://github.com/lik3as";
 
+export const DOCKER_COLOR = "#0db7ed";
+export const TS_COLOR = "#007acc";
+export const NGINX_COLOR  = "#009900";
+export const GIT_COLOR  = "#f1502f";
+export const SQLZ_COLOR  = "#03afef";
+export const SC_COLOR  = "#ED0588";
+export const REACT_COLOR  = "#0A6ED1";
+export const PSQL_COLOR  = "#0064A5";
+
 export const HEADER_HEIGHT = 62;
+export const PADDING_MAIN = 16;
