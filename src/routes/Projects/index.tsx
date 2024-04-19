7dc20d1f20c8523ca1f9467f8f058d6d1341f3ee
lik3as <daviacosta2006@gmail.com>
2024-04-18 23:45:06 -0300

    add docker to idface project

--- src/routes/Projects/index.tsx
+++ src/routes/Projects/index.tsx
@@ -1,11 +1,11 @@
 import { FC, useState } from "react";
 import { Card } from "react-bootstrap";
 import { FaCircleArrowRight } from "react-icons/fa6";
-import { SiTypescript, SiNextdotjs, SiStyledcomponents, SiReact, SiPostgresql, SiSequelize, SiNginx } from "react-icons/si";
+import { SiTypescript, SiNextdotjs, SiStyledcomponents, SiReact, SiPostgresql, SiSequelize, SiNginx, SiDocker } from "react-icons/si";
 
 import StyledProjects, { ImgCtn, List, TechItem } from "./styles";
 import Project from "../../types/Project";
-import { NGINX_COLOR, PSQL_COLOR, REACT_COLOR, SC_COLOR, SQLZ_COLOR, TS_COLOR } from "../../constants";
+import { DOCKER_COLOR, NGINX_COLOR, PSQL_COLOR, REACT_COLOR, SC_COLOR, SQLZ_COLOR, TS_COLOR } from "../../constants";
 import ncaMain from "../../assets/images/nca-main.webp"
 import idfaceMain from "../../assets/images/idface-main.webp"
 
@@ -38,7 +38,7 @@ const projects: Project[] =  [
         {Icon: SiStyledcomponents, color: SC_COLOR}
       ],
       back: [
-        {Icon: SiNginx, color: NGINX_COLOR}
+        {Icon: SiNginx, color: NGINX_COLOR}, {Icon: SiDocker, color: DOCKER_COLOR}
       ]
     }
   }
