f1dc5bf7080b03267418dc167387266ae9df49fb
lik3as <daviacosta2006@gmail.com>
2024-12-14 23:23:42 -0300

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://lik3as.github.io/portfolio/",
+  base: "/portfolio/",
   plugins: [
     react({
       babel: {
