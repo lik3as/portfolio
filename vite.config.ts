4e7026075311f29f2d59524936ef6367bf022301
lik3as <daviacosta2006@gmail.com>
2024-12-14 23:01:51 -0300

    chore: modified base url to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://lik3as.github.io/portfolio/",
   plugins: [
     react({
       babel: {
